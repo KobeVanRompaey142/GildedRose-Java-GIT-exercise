9c72e888b0ddb720625197e5e39d4fabdb8c5ee8
KobeVanRompaey142 <75724254+KobeVanRompaey142@users.noreply.github.com>
2020-12-20 15:27:07 +0100

    Git commit message hereHb

---
+++ src/main/java/com/gildedrose/items/SwitchItem.java
@@ -0,0 +1,30 @@
+package com.gildedrose;
+
+public class SwitchItem extends Item{
+    public SwitchItem(String naam, int sellIn, int quality) {
+        super(naam, sellIn, quality);
+    }
+
+    @Override
+    public void updateQuality() {
+        int factor;
+        if (this.sellIn >= 0) {
+            factor = 1;
+            this.quality = Math.min(this.quality + factor, 50);
+        }
+        else{
+            factor = 1;
+            this.quality = Math.max(this.quality - factor, 0);
+        }
+
+
+    }
+}
+/*
+Een SwitchItem is een item dat, zolang de sellInValue groter of gelijk aan 0 is,
+ elke dag in kwaliteit (1) omhoog gaat maar nooit boven de 50 gaat.
+Maar het is ook een item dat, zodra de sellInValue kleinder dan 0 is,
+elke dag in kwaliteit (1) afneemt maar nooit onder 0 gaat
+Zorg dat het Item zich zo gedraagt en schrijf ook een test hiervoor
+ waarbij je 2 functies maakt (eentje voor sellInValue >= 0 en eentje voor < 0)
+ */
